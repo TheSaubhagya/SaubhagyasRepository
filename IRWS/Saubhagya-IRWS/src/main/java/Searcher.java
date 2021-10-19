e7c249806237ce15daada05ecd6b6c5af9ba990d
TheSaubhagya <57622683+TheSaubhagya@users.noreply.github.com>
2021-10-19 17:44:27 +0100

    Add files via upload

---
+++ IRWS/Saubhagya-IRWS/src/main/java/Searcher.java
@@ -0,0 +1,135 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * The base source code from the Apache website was modified as part of my submission for
+ * the module 'Information Retrieval and Web Search (CS7IS3)' in Trinity College Dublin.
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.PrintWriter;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.core.SimpleAnalyzer;
+import org.apache.lucene.analysis.core.WhitespaceAnalyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.analysis.en.EnglishAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.queryparser.classic.MultiFieldQueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.search.similarities.*;
+import org.apache.lucene.store.FSDirectory;
+
+/** Simple command-line based search demo. */
+public class Searcher {
+
+    public void searchMethod() throws Exception {
+
+        String index = "index";
+        String queryString = "";
+
+        IndexReader reader = DirectoryReader.open(FSDirectory.open(Paths.get(index)));
+        IndexSearcher searcher = new IndexSearcher(reader);
+
+        //Analyzer analyzer = new SimpleAnalyzer();
+        //Analyzer analyzer = new WhitespaceAnalyzer();
+        //Analyzer analyzer = new StandardAnalyzer();
+        Analyzer analyzer = new EnglishAnalyzer();
+
+        String results_path = "results.txt";
+        PrintWriter writer = new PrintWriter(results_path, "UTF-8");
+
+        //BM25 Similarity
+        searcher.setSimilarity(new BM25Similarity());
+
+        //Classic Similarity
+        //searcher.setSimilarity(new ClassicSimilarity());
+
+        //LMDirichletSimilarity
+        //searcher.setSimilarity(new LMDirichletSimilarity());
+
+        //Trying a multi similarity model
+        //searcher.setSimilarity(new MultiSimilarity(new Similarity[]{new BM25Similarity(),new ClassicSimilarity()}));
+
+        //Trying another multi similarity model
+        //searcher.setSimilarity(new MultiSimilarity(new Similarity[]{new BM25Similarity(),new LMDirichletSimilarity()}));
+
+        //Trying another multi similarity model
+        //searcher.setSimilarity(new MultiSimilarity(new Similarity[]{new ClassicSimilarity(),new LMDirichletSimilarity()}));
+
+        String queriesPath = "cran/cran.qry";
+        BufferedReader bufferedReader = Files.newBufferedReader(Paths.get(queriesPath), StandardCharsets.UTF_8);
+        MultiFieldQueryParser parser = new MultiFieldQueryParser(new String[]{"title", "author", "bibliography", "words"}, analyzer);
+
+        String currentLine = bufferedReader.readLine();
+
+        System.out.println("Reading in queries and creating search results.");
+
+        String id = "";
+        int i=0;
+
+        while (currentLine != null) {
+            i++;
+            if (currentLine.startsWith(".I")) {
+                id = Integer.toString(i);
+                currentLine = bufferedReader.readLine();
+            }
+            if (currentLine.startsWith(".W")) {
+                currentLine = bufferedReader.readLine();
+                while (currentLine != null && !currentLine.startsWith(".I")) {
+                    queryString += currentLine + " ";
+                    currentLine = bufferedReader.readLine();
+                }
+            }
+            queryString = queryString.trim();
+            Query query = parser.parse(QueryParser.escape(queryString));
+            queryString = "";
+            performSearch(searcher, writer, Integer.parseInt(id), query);
+        }
+
+        System.out.println("Results have been written to the 'results.txt' file.");
+        writer.close();
+        reader.close();
+    }
+
+
+    // Performs search and writes results to the writer
+    public static void performSearch(IndexSearcher searcher, PrintWriter writer, Integer queryNumber, Query query) throws IOException {
+        /*
+         * After a bit of analysis, I found that since the dataset is pretty small,
+         * the number of top hits can be kept at 1400 (which is the total no. of docs)
+         * but to be on the safe side, I left it at 999 to keep the efficiency high
+         * as per the documentation.
+         */
+        TopDocs results = searcher.search(query, 999);
+        ScoreDoc[] hits = results.scoreDocs;
+
+        // To write the results for each hit in the format expected by the trec_eval tool.
+        for (int i = 0; i < hits.length; i++) {
+            Document doc = searcher.doc(hits[i].doc);
+            writer.println(queryNumber + " 0 " + doc.get("id") + " " + i + " " + hits[i].score + " Saubhagya");
+        }
+    }
+}
