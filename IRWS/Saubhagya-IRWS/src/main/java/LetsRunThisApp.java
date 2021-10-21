b0cffc0b6cb422c76f558882315860f1e3e6e4e1
TheSaubhagya <57622683+TheSaubhagya@users.noreply.github.com>
2021-10-21 16:21:55 +0100

    Update and rename App.java to LetsRunThisApp.java

--- IRWS/Saubhagya-IRWS/src/main/java/LetsRunThisApp.java
+++ IRWS/Saubhagya-IRWS/src/main/java/LetsRunThisApp.java
@@ -2,7 +2,7 @@
  * An entry point to the entire program with the 'main()' method.
  */
 
-public class App {
+public class LetsRunThisApp {
     public static void main (String[] args) throws Exception {
         Indexer letsinsdex = new Indexer();
         Searcher letssearch = new Searcher();
